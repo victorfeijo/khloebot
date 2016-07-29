c50c27bdbc3c238c27785cb773f133d4402e9ad9
Victor Feijo <victor.feijoa@gmail.com>
2016-07-28 21:11:58 -0300

    syntax :snake:

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const TelegramBot = require('node-telegram-bot-api');
 const Handler = require('./handler');
 
-var token = '232462923:AAEIimUDnetUqJEs16_73MvYiY066nHmLx0';
+const token = '232462923:AAEIimUDnetUqJEs16_73MvYiY066nHmLx0';
 
-var bot = new TelegramBot(token, {polling: true});
-var handler = new Handler();
+let bot = new TelegramBot(token, {polling: true});
+let handler = new Handler();
 
 bot.onText(/\/e (hello)/, function(msg, match) {
   handler.hello(match, bot, msg);
