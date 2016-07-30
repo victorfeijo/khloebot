e4d67e9fb9dc2ec8fc0e8b28d98a5e39c6a44912
Victor Feijo <victor.feijoa@gmail.com>
2016-07-29 22:15:43 -0300

    now given tips about movie famous and talking with duckduckgo :bird:

--- handler.js
+++ handler.js
@@ -1,42 +1,84 @@
 const request = require('request');
 
-let Handler = function() {};
+class Handler {
 
-Handler.prototype.hello = function(match, bot, msg) {
-  let resp = match[1];
+  constructor(bot) {
+    this.bot = bot;
+  }
 
-  bot.sendMessage(msg.from.id, resp);
-};
+  hello(match, msg) {
+    let resp = match[1];
 
-Handler.prototype.ru = function(bot, msg) {
-  request('https://voucomerno.ru/menu.json', function(error, response, body) {
-    if (!error && response.statusCode == 200){
-      let menu = JSON.parse(body);
-      let parsedMenu = menu.main + '\n' +
-                       menu.complement + '\n' +
-                       menu.salad + '\n' +
-                       menu.dessert
+    this.bot.sendMessage(msg.from.id, resp);
+  };
 
-      bot.sendMessage(msg.from.id, parsedMenu);
-    }
-    else {
-      bot.sendMessage(msg.from.id, 'Failed :(')
-    }
-  })
-};
+  ru(msg) {
+    let bot = this.bot;
+    request('https://voucomerno.ru/menu.json', function(error, response, body) {
+      if (!error && response.statusCode == 200){
+        let menu = JSON.parse(body);
+        let parsedMenu = menu.main + '\n' +
+                        menu.complement + '\n' +
+                        menu.salad + '\n' +
+                        menu.dessert;
 
-Handler.prototype.tip = function(bot, msg) {
-  request({
-    headers: {
-      "X-Mashape-Key": "zFM4272ikMmshzZ16RhJLcTqS7yAp1pcZR8jsnTZ9e5qld2aZn",
-      "Content-Type": "application/x-www-form-urlencoded",
-      "Accept": "application/json"
-    },
-    uri: "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=movies",
-    methid: "POST"
-  }, function(err, res, body) {
-    console.log(body)
-  })
+        bot.sendMessage(msg.from.id, parsedMenu);
+      }
+      else {
+        bot.sendMessage(msg.from.id, 'Failed :(')
+      }
+    })
+  };
+
+  tip(msg) {
+    let bot = this.bot;
+    request({
+      headers: {
+        "X-Mashape-Key": "zFM4272ikMmshzZ16RhJLcTqS7yAp1pcZR8jsnTZ9e5qld2aZn",
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Accept": "application/json"
+      },
+      uri: "https://andruxnet-random-famous-quotes.p.mashape.com/",
+      form: { cat: 'famous' },
+      method: "POST"
+    }, function(error, response, body) {
+      if (!error && response.statusCode == 200){
+        let tip = JSON.parse(body);
+        let parsedTip = tip.quote + '\n' +
+                        'Author: ' + tip.author;
+
+        bot.sendMessage(msg.from.id, parsedTip);
+      }
+      else {
+        bot.sendMessage(msg.from.id, 'Failed :(');
+      }
+    })
+  };
+
+  whatIs(msg, match) {
+    let bot = this.bot;
+    request({
+      headers: {
+        "X-Mashape-Key": "zFM4272ikMmshzZ16RhJLcTqS7yAp1pcZR8jsnTZ9e5qld2aZn",
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Accept": "application/json"
+      },
+      uri: "https://duckduckgo-duckduckgo-zero-click-info.p.mashape.com/",
+      form: { format: 'json', q: match[1] },
+      method: "POST"
+    }, function(error, response, body) {
+      if (!error && response.statusCode == 200){
+        let result = JSON.parse(body);
+        console.log(result);
+        let parsedResult = result.RelatedTopics[0].Text;
+
+        bot.sendMessage(msg.from.id, parsedResult);
+      }
+      else {
+        bot.sendMessage(msg.from.id, 'Failed :(');
+      }
+    })
+  };
 };
 
 module.exports = Handler;
--- index.js
+++ index.js
@@ -4,16 +4,20 @@ const Handler = require('./handler');
 const token = '232462923:AAEIimUDnetUqJEs16_73MvYiY066nHmLx0';
 
 let bot = new TelegramBot(token, {polling: true});
-let handler = new Handler();
+let handler = new Handler(bot);
 
-bot.onText(/\/e (hello)/, function(msg, match) {
-  handler.hello(match, bot, msg);
+bot.onText(/\/e (.*)/, function(msg, match) {
+  handler.hello(match, msg);
 });
 
 bot.onText(/(ru)/, function(msg, match) {
-  handler.ru(bot, msg);
+  handler.ru(msg);
 });
 
 bot.onText(/(tip)/, function(msg, match) {
-  handler.tip(bot, msg);
+  handler.tip(msg);
+});
+
+bot.onText(/What is (.*)/, function(msg, match) {
+  handler.whatIs(msg, match);
 });
